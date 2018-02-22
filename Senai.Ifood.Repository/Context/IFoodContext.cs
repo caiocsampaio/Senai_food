9ff61617521d26fd056f7c7c5617a431e65196ce
Caio Sampaio <caio.chaves.sampaio@gmail.com>
2018-02-22 19:29:39 -0300

    Second Commit

--- Senai.Ifood.Repository/Context/IFoodContext.cs
+++ Senai.Ifood.Repository/Context/IFoodContext.cs
@@ -1,9 +1,17 @@
 using Microsoft.EntityFrameworkCore;
+using Senai.Ifood.Domain.Entities;
 
 namespace Senai.Ifood.Repository.Context
 {
     public class IFoodContext : DbContext
     {
-        public DbSet<> MyProperty { get; set; }
+        public IFoodContext(DbContextOptions<IFoodContext> options) : base(options){}
+        public DbSet<ClienteDomain> Clientes { get; set; }
+        public DbSet<EspecialidadeDomain> Especialidades { get; set; }
+        public DbSet<PermissaoDomain> Permissoes { get; set; }
+        public DbSet<ProdutoDomain> Produtos { get; set; }
+        public DbSet<RestauranteDomain> Restaurates { get; set; }
+        public DbSet<UsuarioDomain> Usuarios { get; set; }
+        public DbSet<UsuarioPermissaoDomain> UsuariosPermissoes { get; set; }
     }
 }
