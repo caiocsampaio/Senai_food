1cc47623385004b1f3b0b68eb5fbc6889d9bc2ac
Caio Sampaio <caio.chaves.sampaio@gmail.com>
2018-02-22 20:06:09 -0300

    IntelliSense Correct; Api.Startup.cs

--- Senai.Ifood.Repository/Context/IFoodContext.cs
+++ Senai.Ifood.Repository/Context/IFoodContext.cs
@@ -10,8 +10,20 @@ namespace Senai.Ifood.Repository.Context
         public DbSet<EspecialidadeDomain> Especialidades { get; set; }
         public DbSet<PermissaoDomain> Permissoes { get; set; }
         public DbSet<ProdutoDomain> Produtos { get; set; }
-        public DbSet<RestauranteDomain> Restaurates { get; set; }
+        public DbSet<RestauranteDomain> Restaurantes { get; set; }
         public DbSet<UsuarioDomain> Usuarios { get; set; }
         public DbSet<UsuarioPermissaoDomain> UsuariosPermissoes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<ClienteDomain>().ToTable("Clientes");
+            modelBuilder.Entity<EspecialidadeDomain>().ToTable("Especialidades");
+            modelBuilder.Entity<PermissaoDomain>().ToTable("Permissoes");
+            modelBuilder.Entity<ProdutoDomain>().ToTable("Produtos");
+            modelBuilder.Entity<RestauranteDomain>().ToTable("Restaurantes");
+            modelBuilder.Entity<UsuarioDomain>().ToTable("Usuarios");
+            modelBuilder.Entity<UsuarioPermissaoDomain>().ToTable("UsuariosPermissoes");
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
--- Senai.Ifood.WebApi/Startup.cs
+++ Senai.Ifood.WebApi/Startup.cs
@@ -5,16 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Senai.Ifood.Repository.Context;
+using static Microsoft.AspNetCore.Hosting.Internal.HostingApplication;
 
 namespace Senai.Ifood.WebApi
 {
     public class Startup
     {
+        public IConfiguration _configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            configuration = _configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<IFoodContext>(options => 
+                options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
